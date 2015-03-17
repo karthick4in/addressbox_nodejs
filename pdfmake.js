f035b02cb35edf4324807cdf03d80164d933c80e
karthick4in <karthick4in@gmail.com>
2015-03-17 17:31:22 +0530

    initial update
    initial update

---
+++ pdfmake.js
@@ -0,0 +1,12 @@
+phantom = require('phantom');
+
+phantom.create(function(ph){
+  ph.createPage(function(page) {
+    page.open("http://www.google.com", function(status) {
+      page.render('google.pdf', function(){
+        console.log('Page Rendered');
+        ph.exit();
+      });
+    });
+  });
+});
